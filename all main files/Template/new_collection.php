c6f2005e7dfa2b87767f26abe218a4e6a0d47076
Abdullah_assoli <abdullahassoli@gmail.com>
2022-05-15 04:58:08 +0200

    link the products with database

--- all main files/Template/new_collection.php
+++ all main files/Template/new_collection.php
@@ -1,9 +1,14 @@
+<?php
+  $product_shuffle=$product->getData();
+  shuffle($product_shuffle);
+?>
 <div class="w-full flex flex-col items-center justify-center mt-9" id="newCollection">
   <h2 class="text-center font-bold text-2xl my-3">.New Collection</h2>
 <div class="container flex flex-row flex-wrap gap-4 justify-center items-center " >
-  <div class="shop-item md:w-70 bg-gray-100 flex flex-col items-center p-1 pb-2 sm:w-60 ">
-    <div class="item-img bg-[url('./assets/pr-1.jpg')] bg-cover bg-center w-full h-72"></div>
-    <h2 class="item-name my-2">Fairy tail collection</h2>
+<?php foreach($product_shuffle as $item){ ?>
+<div class="shop-item md:w-96 bg-gray-100 flex flex-col items-center p-1 pb-2 sm:w-60 ">
+    <div class="item-img bg-[url('<?php echo $item["item_image"] ?? "./assets/pr-1.jpg"; ?>')] bg-cover bg-center w-full h-72"></div>
+    <h2 class="item-name my-2"><?php echo $item["item_name"]?? "Unknown"; ?></h2>
     <p class="item-stars">
       <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
       <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
@@ -12,50 +17,9 @@
       <span class="text-yellow-300"><i class="fa-regular fa-star"></i></span>
     
     </p>
-    <p class="item-price font-bold"><del class="font-light text-gray-500">  $200</del> 150$</p>
+    <p class="item-price font-bold">$<?php echo $item["item_price"]+20 ?? "0" ?></p>
     <button class="item-button rounded border bg-blue-700 hover:bg-yellow-300 p-1 text-white hover:text-black"><a href="./product.php">Add to Cart</a></button>
   </div>
-  <div class="shop-item md:w-70 bg-gray-100 flex flex-col items-center p-1 pb-2 sm:w-60">
-    <div class="item-img bg-[url('./assets/pr-2.jpg')] bg-cover bg-center w-full h-72"></div>
-    <h2 class="item-name my-2">Bleach collection</h2>
-    <p class="item-stars">
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-regular fa-star"></i></span>
-    
-    </p>
-    <p class="item-price font-bold"><del class="font-light text-gray-500">  $200</del> 150$</p>
-    <button class="item-button rounded border bg-blue-700 hover:bg-yellow-300 p-1 text-white hover:text-black"><a href="./product.php">Add to Cart</a></button>
-  </div>
-  <div class="shop-item md:w-70 bg-gray-100 flex flex-col items-center p-1 pb-2 sm:w-60">
-    <div class="item-img bg-[url('./assets/pr-3.jpg')] bg-cover bg-center w-full h-72"></div>
-    <h2 class="item-name my-2">Naruto collection</h2>
-    <p class="item-stars">
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-regular fa-star"></i></span>
-    
-    </p>
-    <p class="item-price font-bold"><del class="font-light text-gray-500">  $200</del> 150$</p>
-    <button class="item-button rounded border bg-blue-700 hover:bg-yellow-300 p-1 text-white hover:text-black"><a href="./product.php">Add to Cart</a></button>
-  </div>
-  <div class="shop-item md:w-70 bg-gray-100 flex flex-col items-center p-1 pb-2 sm:w-60">
-    <div class="item-img bg-[url('./assets/pr-3.jpg')] bg-cover bg-center w-full h-72"></div>
-    <h2 class="item-name my-2">Naruto collection</h2>
-    <p class="item-stars">
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-solid fa-star"></i></span>
-      <span class="text-yellow-300"><i class="fa-regular fa-star"></i></span>
-    
-    </p>
-    <p class="item-price font-bold"><del class="font-light text-gray-500">  $200</del> 150$</p>
-    <button class="item-button rounded border bg-blue-700 hover:bg-yellow-300 p-1 text-white hover:text-black"><a href="./product.php">Add to Cart</a></button>
-  </div>
+  <?php } ?>
 </div>
 </div>
